b5522f33eb902173ddb07615c7c2fb11f7dd0a44
Maciej Gawłowski <46463400+mmgawlowski@users.noreply.github.com>
2019-04-10 20:11:01 +0200

    Update Palindrome_3.py

--- Palindrome_3.py
+++ Palindrome_3.py
@@ -1,11 +1,12 @@
 # Simpler version of Palindrome.py without defining a new function
 while True:
   word = input('Wprowadź słowo, które chcesz sprawdzić\n')
-  n = len(word)
+  word_t = word.lower().replace(' ', '')
+  n = len(word_t)
   result = (word + ' jest palindromem')
 
   for i in range(n):
-    if word[i] != word[-1-i]:
+    if word_t[i] != word_t[-1-i]:
       result = (word + ' nie jest palindromem')
 
   print(result)
