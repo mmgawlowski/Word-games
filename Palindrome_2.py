dad7c1ceee85efb9f5be485e2a99429226dd6c5a
Maciej Gawłowski <46463400+mmgawlowski@users.noreply.github.com>
2019-04-10 20:00:40 +0200

    Update Palindrome_2.py

--- Palindrome_2.py
+++ Palindrome_2.py
@@ -1,7 +1,7 @@
 while True:
     word = input('Wprowadź słowo, które chcesz sprawdzić\n')
     rev = word[::-1] # [::-1] reverses whole string
-    if word.lower() != rev.lower():
+    if word.lower().replace(' ', '') != rev.lower().replace(' ', ''): # .lower() and .replace() make function case and whitespace insensitive
         print(word + ' nie jest palindromem')
         continue
     else:
