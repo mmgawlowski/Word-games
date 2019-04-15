8125b07ea57a16bb547827214af26e88c30e2b50
Maciej Gawłowski <46463400+mmgawlowski@users.noreply.github.com>
2019-04-15 23:15:23 +0200

    Update Palindrom.py

--- Palindrom.py
+++ Palindrom.py
@@ -22,7 +22,7 @@ while True:
     if Palindrome(word_t) == False:
         print(word + ' nie jest palindromem')
         continue
-    elif re.search('[a-zA-Z ]|^$', word) == None :
+    elif re.search('[a-zA-Z ]|^$', word) == None:
         print('Znak interpunkcyjny nie jest palindromem')
     else:
         print(word + ' jest palindromem')
