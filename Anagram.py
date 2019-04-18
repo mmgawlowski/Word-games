4cf160b2389a1ea18d6a094512231d698893e9b3
Maciej Gawłowski <46463400+mmgawlowski@users.noreply.github.com>
2019-04-18 15:56:49 +0200

    Update Anagram.py
    Introduced improvements in recognition of correct form of anagram assuming it should contain only letters, spaces and punctuation marks.

--- Anagram.py
+++ Anagram.py
@@ -7,21 +7,39 @@ def anagram(word_1, word_2):
   n = len(word_1)
   
   for i in range(n):
-    if re.findall(word_1[i], word_1) != re.findall(word_1[i], word_2):
+    if re.findall(word_1[i], word_1) != re.findall(word_1[i], word_2,):
       return False
   return True
 
 while True:
   
-  word_1 = input('Wprowadź wyrażenie, które chcesz sprawdzić:\n')
-  word_2 = input('Wprowadź wyrażenie, które ma być porównane z pierwszym:\n')
+  while True:
+    word_1 = input('Wprowadź wyrażenie, które chcesz sprawdzić:\n')
+    if re.search(r'[0-9~@#$%^&*+=<>{}~/|\[\]\\]', word_1) != None:
+      print('Wprowadzone wyrażenie powinno zawierać jedynie litery alfabetu i znaki interpunkcyjne')
+      continue
+    elif re.search('[a-zA-Z ]|^$', word_1) == None:
+      print('Wyrażenie nie powinno składać się wyłącznie ze znaków interpunkcyjnych')
+      continue
+    else:
+      word_1_t = ''.join(x for x in word_1 if x.isalpha()).lower()
+      break
+
+  while True:
+    word_2 = input('Wprowadź wyrażenie, które ma być porównane z pierwszym:\n')
+    if re.search(r'[0-9~@#$%^&*+=<>{}~/|\[\]\\]', word_2) != None:
+      print('Wprowadzone wyrażenie powinno zawierać jedynie litery alfabetu i znaki interpunkcyjne')
+      continue
+    elif re.search('[a-zA-Z ]|^$', word_2) == None:
+      print('Wyrażenie nie powinno składać się wyłącznie ze znaków interpunkcyjnych')
+      continue
+    else:
+      word_2_t = ''.join(x for x in word_2 if x.isalpha()).lower()
+      break
   
-  word_1_t = ''.join(x for x in word_1 if x.isalpha()).lower()
-  word_2_t = ''.join(x for x in word_2 if x.isalpha()).lower()
-  
-  if anagram(word_1_t, word_2_t) == True:
-    print(word_1 + ' jest anagramem ' + word_2)
+  if anagram(word_1_t, word_2_t) == False:
+    print(word_1 + ' nie jest anagramem ' + word_2)
     continue
   else:
-    print(word_1 + ' nie jest anagramem ' + word_2)
+    print(word_1 + ' jest anagramem ' + word_2)
     continue
